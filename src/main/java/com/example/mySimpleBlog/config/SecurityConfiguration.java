9f34e96b767f763bbe699308c2174619f55dd86e
Chris G <12830070+gruemme@users.noreply.github.com>
2023-07-12 10:45:48 +0200

    Updated Spring security syntag

--- src/main/java/com/example/mySimpleBlog/config/SecurityConfiguration.java
+++ src/main/java/com/example/mySimpleBlog/config/SecurityConfiguration.java
@@ -3,15 +3,16 @@ package com.example.mySimpleBlog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
 
     private static final String[] PATHS_WITH_FREE_ACCESS = {
@@ -23,14 +24,17 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/me").authenticated()
-                .requestMatchers(HttpMethod.POST, "/entry").authenticated()
-                .requestMatchers(PATHS_WITH_FREE_ACCESS).permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> {
+                    authorize
+                            .requestMatchers("/me").authenticated()
+                            .requestMatchers(HttpMethod.POST, "/entry").authenticated()
+                            .requestMatchers(PATHS_WITH_FREE_ACCESS).permitAll()
+                            .anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
